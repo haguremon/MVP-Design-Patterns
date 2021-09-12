364ee6fb4bef0fe770b0d18defa1592a7c032548
Yuta iwasaki <yanyong0127@gmail.com>
2021-09-12 16:41:54 +0900

    眠いから起きてからやるかも

---
+++ MVP  Design Patterns/Models/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  MVP  Design Patterns
+//
+//  Created by IwasakIYuta on 2021/09/12.
+//
+
+import Foundation
+
+enum Gender: String {
+    case male
+    case female
+    case other
+}
+
+
+struct User {
+    var name: String
+    var email: String
+    var gender: Gender
+}
---
+++ MVP  Design Patterns/Views/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MVP  Design Patterns
+//
+//  Created by IwasakIYuta on 2021/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        messageLabel.isHidden = true
+    }
+    
+    //MARK -submitDate
+    @IBAction func submitDateButton(_ sender: Any) {
+    
+    
+    }
+    
+
+
+}
+
